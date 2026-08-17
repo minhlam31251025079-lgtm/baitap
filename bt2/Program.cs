365f21e977ea126a71b76a7182d6842a3c591af8
PC <PC@LAPTOP-6BVJIF37>
2026-08-17 15:43:06 +0700

    Add project files.

---
+++ bt2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("nhap so thu nhat:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("nhap so thu hai:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int sum = a + b;
+  Console.WriteLine($"tong hai so la: {sum}");
---
+++ bt2/bt22.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bt2
+{
+    internal class bt22
+    {
+    }
+}
+Console.WriteLine("nhap so a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("nhao so b:");
+int b = Convert.ToInt32(Console.ReadLine());
