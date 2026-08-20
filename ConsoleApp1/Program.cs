476c6670996eba62aeefdd7537fc259ef9a428b4
Minh Lâm <minhlam.31251025079@st.ueh.edu.vn>
2026-08-21 01:42:31 +0700

    bt 2 bai 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so thuc thu nhat: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap so thuc thu hai: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double tích = a * b;
+            Console.WriteLine($"Tich hai so thuc la: {tích}");
+        }
+    }
+}
