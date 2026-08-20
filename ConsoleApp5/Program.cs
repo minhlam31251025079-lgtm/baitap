6413aae66c184230b4bfad37795a4c834189addb
Minh Lâm <minhlam.31251025079@st.ueh.edu.vn>
2026-08-21 01:59:03 +0700

    bt 2 bai 4,5,6,7,8,910

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Size of int: {sizeof(int)} bytes");
+            Console.WriteLine($"Size of float: {sizeof(float)} bytes");
+            Console.WriteLine($"Size of double: {sizeof(double)} bytes");
+            Console.WriteLine($"Size of char: {sizeof(char)} bytes");
+            Console.WriteLine($"Size of bool: {sizeof(bool)} bytes");
+        }
+    }
+}
---
+++ baitap2bai10.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap2bai10.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so ngay: ");
+            int totalDays = Convert.ToInt32(Console.ReadLine());
+
+            int years = totalDays / 365;
+            int remainingDaysAfterYears = totalDays % 365;
+
+            int weeks = remainingDaysAfterYears / 7;
+            int days = remainingDaysAfterYears % 7;
+
+            Console.WriteLine($"{totalDays} ngay = {years} nam, {weeks} tuan, va {days} ngay");
+        }
+    }
+}
